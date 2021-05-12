1d217acff51b5a9ffcd5df477ccb1592186f7b73
Patryk Woziński <patryk.wozinski@gmail.com>
2021-05-12 16:17:18 +0200

    working simple impl

--- lib/churn.ex
+++ lib/churn.ex
@@ -3,7 +3,9 @@ defmodule Churn do
   Churn is a good refactoring candidates finder for your version-control system.
   """
 
-  def run(args) do
+  def run(_args) do
+    IO.puts("Hello World")
+
     :ok
   end
 end
