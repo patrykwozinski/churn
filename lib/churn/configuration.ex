c48bd0c48ab44258cc5ef97e31ce6651343a7863
Patryk Woziński <patryk.wozinski@gmail.com>
2021-05-12 16:24:41 +0200

    init of config

---
+++ lib/churn/configuration.ex
@@ -0,0 +1,21 @@
+defmodule Churn.Configuration do
+  @moduledoc """
+  Represents the whole configuration of Churn.
+  It's used for every run.
+  """
+
+  use TypedStruct
+
+  typedstruct enforce: true do
+    field(:min_score_to_show, pos_integer())
+    field(:commit_since, String.t())
+    field(:directories_to_scan, list(String.t()))
+    field(:file_extensions, list(String.t()))
+    field(:files_to_ignore, list(String.t()))
+  end
+
+  @spec build() :: %__MODULE__.t()
+  def build(args \\ []) when is_list(args) do
+
+  end
+end
