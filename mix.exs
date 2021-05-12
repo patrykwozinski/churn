1e1814a9192bb78581b6c8c4407ed916de77a632
Patryk Woziński <patryk.wozinski@gmail.com>
2021-05-12 15:51:38 +0200

    mix.exs improvements

--- mix.exs
+++ mix.exs
@@ -16,18 +16,24 @@ defmodule Churn.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :typed_struct]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:typed_struct, "~> 0.2"},
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
     ]
   end
 end
