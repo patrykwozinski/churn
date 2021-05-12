e9e086f95094854cd80d3f5d95405a274595a0e4
Patryk Woziński <patryk.wozinski@gmail.com>
2021-05-12 15:50:48 +0200

    dialyzer project def

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule Churn.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_add_deps: :apps_direct,
+        plt_add_apps: [],
+        plt_ignore_apps: [:logger]
+      ]
     ]
   end
 
