821b5cb926837ba77c59b786371b091cfb560b7b
Patryk Woziński <patryk.wozinski@gmail.com>
2021-05-12 19:51:23 +0200

    default params

--- lib/churn/configuration.ex
+++ lib/churn/configuration.ex
@@ -14,13 +14,19 @@ defmodule Churn.Configuration do
     field(:files_to_ignore, list(String.t()))
   end
 
+  @default_minimum_score_to_show 0.1
+  @default_commit_since "1 year ago"
+  @default_directories_to_scan []
+  @default_file_extensions [".ex"]
+  @default_files_to_ignore []
+
   @spec build(list(any())) :: t()
   def build(args \\ []) when is_list(args) do
-    min_score_to_show = Keyword.fetch!(args, :min_score_to_show)
-    commit_since = Keyword.fetch!(args, :commit_since)
-    directories_to_scan = Keyword.fetch!(args, :directories_to_scan)
-    file_extensions = Keyword.fetch!(args, :file_extensions)
-    files_to_ignore = Keyword.fetch!(args, :files_to_ignore)
+    min_score_to_show = Keyword.get(args, :min_score_to_show, @default_minimum_score_to_show)
+    commit_since = Keyword.get(args, :commit_since, @default_commit_since)
+    directories_to_scan = Keyword.get(args, :directories_to_scan, @default_directories_to_scan)
+    file_extensions = Keyword.get(args, :file_extensions, @default_file_extensions)
+    files_to_ignore = Keyword.get(args, :files_to_ignore, @default_files_to_ignore)
 
     %__MODULE__{
       min_score_to_show: min_score_to_show,
