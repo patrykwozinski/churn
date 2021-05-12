1a9c70350e2be9a56a76a527a14faa343d379b48
Patryk Woziński <patryk.wozinski@gmail.com>
2021-05-12 23:37:32 +0200

    times_changed must be an integer

--- lib/churn/processor/result.ex
+++ lib/churn/processor/result.ex
@@ -8,10 +8,10 @@ defmodule Churn.Processor.Result do
   typedstruct enforce: true do
     field(:file, File.t())
     field(:complexity, pos_integer())
-    field(:times_changed, non_neg_integer())
+    field(:times_changed, pos_integer())
   end
 
-  @spec build(File.t(), pos_integer(), non_neg_integer()) :: t()
+  @spec build(File.t(), pos_integer(), pos_integer()) :: t()
   def build(file, complexity, times_changed) do
     %__MODULE__{file: file, complexity: complexity, times_changed: times_changed}
   end
