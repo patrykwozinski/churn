dab2c697f9d5810c7964228345c4790cc3c34d4e
Patryk Woziński <patryk.wozinski@gmail.com>
2021-05-12 16:37:19 +0200

    build config

--- lib/churn/configuration.ex
+++ lib/churn/configuration.ex
@@ -7,7 +7,6 @@ defmodule Churn.Configuration do
   use TypedStruct
 
   typedstruct enforce: true do
-    field(:args, list(any()))
     field(:min_score_to_show, pos_integer())
     field(:commit_since, String.t())
     field(:directories_to_scan, list(String.t()))
@@ -15,8 +14,20 @@ defmodule Churn.Configuration do
     field(:files_to_ignore, list(String.t()))
   end
 
-  @spec build() :: %__MODULE__.t()
+  @spec build() :: t()
   def build(args \\ []) when is_list(args) do
+    min_score_to_show = Keyword.fetch!(args, :min_score_to_show)
+    commit_since = Keyword.fetch!(args, :commit_since)
+    directories_to_scan = Keyword.fetch!(args, :directories_to_scan)
+    file_extensions = Keyword.fetch!(args, :file_extensions)
+    files_to_ignore = Keyword.fetch!(args, :files_to_ignore)
 
+    %__MODULE__{
+      min_score_to_show: min_score_to_show,
+      commit_since: commit_since,
+      directories_to_scan: directories_to_scan,
+      file_extensions: file_extensions,
+      files_to_ignore: files_to_ignore
+    }
   end
 end
