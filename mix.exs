9c0156c4d7f31b2065bf50c79760912c4a35dc13
Patryk Woziński <patryk.wozinski@gmail.com>
2021-05-12 15:52:59 +0200

    version fix

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Churn.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :churn,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
