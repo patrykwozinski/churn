057db8b16c392283585f7db02e08023e0c58fc07
Patryk Woziński <patryk.wozinski@gmail.com>
2021-05-15 13:15:33 +0200

    aliases in a description

--- lib/mix/tasks/churn.ex
+++ lib/mix/tasks/churn.ex
@@ -5,10 +5,10 @@ defmodule Mix.Tasks.Churn do
   Run churn analysis of specified directories
 
   Available options:
-  --min-score-to-show 1
-  --commit-since "1 year ago"
-  --directories-to-scan "lib,test"
-  --file-extensions "ex,exs"
+  --min-score-to-show 1 [-s]
+  --commit-since "1 year ago" [-c]
+  --directories-to-scan "lib,test" [-d]
+  --file-extensions "ex,exs" [-e]
   """
   @moduledoc @shortdoc
 
