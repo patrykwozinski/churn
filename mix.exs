01af7bc67cfa2aab94277910b48904793262c6b0
Patryk Woziński <patryk.wozinski@gmail.com>
2021-05-17 20:25:23 +0200

    Use mox library

--- mix.exs
+++ mix.exs
@@ -30,11 +30,12 @@ defmodule Churn.MixProject do
 
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:typed_struct, "~> 0.1"},
-      {:table_rex, "~> 3.1.1"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:mox, "~> 1.0", only: :test},
+      {:table_rex, "~> 3.1.1"},
+      {:typed_struct, "~> 0.1"}
     ]
   end
 
