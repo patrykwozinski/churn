eda6b8cdb08b386472c5ac1adda173e3833458e5
Nathan Long <him@nathanmlong.com>
2021-06-08 14:37:44 -0400

    If no results are found, print a message
    Previously an error was raised

--- lib/churn/renderer/console.ex
+++ lib/churn/renderer/console.ex
@@ -13,7 +13,16 @@ defmodule Churn.Renderer.Console do
     |> Enum.map(fn result ->
       [result.file.path, result.times_changed, result.complexity, result.score]
     end)
-    |> TableRex.quick_render!(header, @title)
+    |> do_render(header, @title)
+  end
+
+  defp do_render([] = _results, _header, _title) do
+    IO.puts "(No results. Please try again with different options.)"
+  end
+
+  defp do_render(results, header, title) do
+    results
+    |> TableRex.quick_render!(header, title)
     |> IO.puts()
   end
 end
